82d54911d39166257b00e824de46f815987470eb
Chris Curro <curro@cooper.edu>
2015-05-08 21:44:55 -0400

    reboot for new non rnn approach

--- env.lua
+++ env.lua
@@ -1,13 +1,45 @@
 require 'nn'
 require 'cutorch'
 require 'cunn'
+-- require 'rnn'
+require 'optim'
 require 'audio'
 require 'signal'
 require 'gnuplot'
 require 'torchx'
+require('./sampleAq.lua')
 
 soundsPath  = '/afs/ee.cooper.edu/courses/ece412/eventDetectionData/singlesounds/sounds'
 
 labelsPath = {}
 labelsPath[1] = '/afs/ee.cooper.edu/courses/ece412/eventDetectionData/singlesounds/annotation1'
 labelsPath[2] = '/afs/ee.cooper.edu/courses/ece412/eventDetectionData/singlesounds/annotation2'
+
+fs = 44.1e3
+superFrameSize = 20e-3*fs
+
+nModel = os.time()
+
+logFile = io.open(string.format('logs/model%d.err',nModel),'a')
+s = torch.initialSeed()
+logFile:write(string.format('Seed: %d\n',s))
+logFile:close()
+
+classCriterion = nn.ClassNLLCriterion()
+classCriterion:cuda()
+
+nEpochs = 4
+epochSize = 400
+epoch = 1
+cvError = torch.Tensor(nEpochs)
+
+optimState = {
+    learningRate = 1e-2,
+    weightDecay = 1e-6, 
+    momentum = 0.9,
+    learningRateDecay = 0
+}
+
+optimMethod = optim.nag
+
+model = 'rnn.lua'
--- loadData.lua
+++ loadData.lua
@@ -1,5 +1,9 @@
 dofile('env.lua')
 
+-- huge refactoring neeeded
+
+print('==> Indexing Audio files')
+
 local soundsFiles = paths.indexdir(soundsPath, 'wav')
 
 local labelsFiles = {}
@@ -22,28 +26,49 @@ local labels = {
 	phone = 13,
 	printer = 14,
 	speech = 15,
-	switch = 16
+	switch = 16,
+	none = 17
 }
 
-function readLabel(filename)
+local function readLabel(filename)
 	-- filename is a string containing the full path to the wav file
 	local name = string.match(filename, "([^/]+)$") -- strip to just filename
 	local name, id = string.match(string.gsub(name,'.wav$',''),'(.-)(%d+)') -- cutoff 
 	local label = labels[name]
-	return label
+	return label, tonumber(id)
 end
+
+local function readSetTimes(filename)
+	local file = io.open(filename)
+	local str = file:read()
+	local setTimes = {}
+	for i in string.gmatch(str, "%S+") do
+		table.insert(setTimes,tonumber(i))
+	end
+	return setTimes
+end	
 
 local dataset = {}
 
+
+print('==> Loading Audio Files')
 for i = 1,soundsFiles:size() do
 	local filename = soundsFiles:filename(i)
 	local p = {} -- sample, label pair
 	p['audio'] = audio.load(filename):select(1,1)
-	p['label'] = readLabel(filename)
+	p['label'], p['id'] = readLabel(filename)
+	local setTimes = {}
+	setTimes[1] = readSetTimes(labelsFiles[1]:filename(i))
+	setTimes[2] = readSetTimes(labelsFiles[2]:filename(i))	
+	p['on']  = (setTimes[1][1] + setTimes[2][1])*0.5
+	p['off'] = (setTimes[1][2] + setTimes[2][2])*0.5
 	table.insert(dataset,p)
 
-	p['audio'] = audio.load(filename):select(1,2)
-	table.insert(dataset,p)
+	-- p['audio'] = audio.load(filename):select(1,2)
+	-- table.insert(dataset,p)
+	if i % 5 == 0 then 
+		xlua.progress(i,soundsFiles:size())
+	end
 end
 
 local function shuffle( t )
@@ -58,18 +83,152 @@ local function shuffle( t )
     end
 end
 
+print('==> Shuffling Dataset')
 shuffle(dataset)
 
-trainingSet = {}
-valSet = {}
+local trainSet = {}
+local valSet = {}
 
 local trainingSplit = .8
 
+print('==> Splitting Dataset')
 for i = 1,#dataset do
 	if i < torch.floor(trainingSplit*#dataset) then
-		table.insert(trainingSet,dataset[i])
+		table.insert(trainSet,dataset[i])
 	else
 		table.insert(valSet,dataset[i])
 	end
 end
 
+-- refactoring needed
+
+local function getSuperFrames(example)
+	-- example is item of dataSet containing 
+	-- the following fields: audio, label, on, off
+    local superFrames = {}
+
+	local onSet  = example['on']*fs
+    local offSet = example['off']*fs
+    for i = 1,torch.floor(example['audio']:numel()/superFrameSize) do
+        local frame = {}
+        frame['label'] = example['label']
+        frame['audio'] = example['audio'][{{1 + (i-1)*superFrameSize,i*superFrameSize}}]
+        if (onSet > 1 + (i-1)*superFrameSize) or (offSet < i*superFrameSize) then
+            frame['event'] = false
+        else
+            frame['event'] = true
+        end
+        if onSet > (1 + (i-1)*superFrameSize) and onSet < (1 + i*superFrameSize) then
+            frame['on'] = true   
+            frame['onSet'] = onSet - (1 + (i-1)*superFrameSize)
+        else
+            frame['on'] = false
+        end
+        if offSet > (1 + (i-1)*superFrameSize) and offSet < (1 + i*superFrameSize) then
+            frame['off'] = true   
+            frame['offSet'] = offSet - (1 + i*superFrameSize)
+        else
+            frame['off'] = false
+        end
+        superFrames[i] = frame
+    end
+    return superFrames
+end
+
+local trainSuperFrames = {}
+
+for i = 1,#trainSet do
+    for k,v in ipairs(getSuperFrames(trainSet[i])) do
+        table.insert(trainSuperFrames, v)
+    end
+end
+
+local valSuperFrames = {}
+
+for i = 1,#valSet do
+    for k,v in ipairs(getSuperFrames(valSet[i])) do
+        table.insert(valSuperFrames, v)
+    end
+end
+
+trainClassSuperFrames = {}
+
+for k,v in ipairs(trainSuperFrames) do
+    local frame = {}
+    if v['event'] then
+        frame['label'] = v['label']
+    else
+        frame['label'] = 17 -- none
+    end
+    frame['audio'] = v['audio']
+    table.insert(trainClassSuperFrames,frame)
+end
+
+shuffle(trainClassSuperFrames)
+
+trainOnRegressSuperFrames = {}
+
+for k,v in ipairs(trainSuperFrames) do
+    local frame = {}
+    if v['on'] then
+        frame['audio'] = v['audio']
+        frame['onSet'] = v['onSet']
+        table.insert(trainOnRegressSuperFrames,frame)
+    end
+end
+
+shuffle(trainOnRegressSuperFrames)
+
+trainOffRegressSuperFrames = {}
+
+for k,v in ipairs(trainSuperFrames) do
+    local frame = {}
+    if v['off'] then
+        frame['audio'] = v['audio']
+        frame['offSet'] = v['offSet']
+        table.insert(trainOffRegressSuperFrames,frame) 
+    end
+end
+
+shuffle(trainOffRegressSuperFrames)
+
+valClassSuperFrames = {}
+
+for k,v in ipairs(valSuperFrames) do
+    local frame = {}
+    if v['event'] then
+        frame['label'] = v['label']
+    else
+        frame['label'] = 17 -- none
+    end
+    frame['audio'] = v['audio']
+    table.insert(valClassSuperFrames,frame)
+end
+
+shuffle(valClassSuperFrames)
+
+valOnRegressSuperFrames = {}
+
+for k,v in ipairs(valSuperFrames) do
+    local frame = {}
+    if v['on'] then
+        frame['audio'] = v['audio']
+        frame['onSet'] = v['onSet']
+        table.insert(valOnRegressSuperFrames,frame)
+    end
+end
+
+shuffle(valOnRegressSuperFrames)
+
+valOffRegressSuperFrames = {}
+
+for k,v in ipairs(valSuperFrames) do
+    local frame = {}
+    if v['off'] then
+        frame['audio'] = v['audio']
+        frame['offSet'] = v['offSet']
+        table.insert(valOffRegressSuperFrames,frame) 
+    end
+end
+
+shuffle(valOffRegressSuperFrames)
